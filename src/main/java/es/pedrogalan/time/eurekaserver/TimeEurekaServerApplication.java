83caa3143f7b185f071f3ae4d1b8413aec1ca0a4
Pedro Galan <pedro.galan@piksel.com>
2020-06-09 19:00:21 +0100

    Spring boot app skeleton

---
+++ src/main/java/es/pedrogalan/time/eurekaserver/TimeEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package es.pedrogalan.time.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeEurekaServerApplication.class, args);
+	}
+
+}
